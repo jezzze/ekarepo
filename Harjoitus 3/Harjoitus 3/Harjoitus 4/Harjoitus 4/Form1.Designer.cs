15c3194636dcd7919b8282671ecb6fe48175aa59
jezzze <69570708+jezzze@users.noreply.github.com>
2021-08-18 16:54:13 +0300

    Add files via upload

---
+++ Harjoitus 3/Harjoitus 3/Harjoitus 4/Harjoitus 4/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace Harjoitus_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SyntymaAikaDT = new System.Windows.Forms.DateTimePicker();
+            this.VuosinaLB = new System.Windows.Forms.Label();
+            this.TunteinaLB = new System.Windows.Forms.Label();
+            this.KuukausinaLB = new System.Windows.Forms.Label();
+            this.MinuutteinaLB = new System.Windows.Forms.Label();
+            this.PaivinaLB = new System.Windows.Forms.Label();
+            this.SekuntteinaLB = new System.Windows.Forms.Label();
+            this.LaskeBT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SyntymaAikaDT
+            // 
+            this.SyntymaAikaDT.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SyntymaAikaDT.Location = new System.Drawing.Point(12, 22);
+            this.SyntymaAikaDT.Name = "SyntymaAikaDT";
+            this.SyntymaAikaDT.Size = new System.Drawing.Size(350, 31);
+            this.SyntymaAikaDT.TabIndex = 0;
+            // 
+            // VuosinaLB
+            // 
+            this.VuosinaLB.AutoSize = true;
+            this.VuosinaLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VuosinaLB.Location = new System.Drawing.Point(22, 56);
+            this.VuosinaLB.Name = "VuosinaLB";
+            this.VuosinaLB.Size = new System.Drawing.Size(0, 25);
+            this.VuosinaLB.TabIndex = 1;
+            this.VuosinaLB.Visible = false;
+            // 
+            // TunteinaLB
+            // 
+            this.TunteinaLB.AutoSize = true;
+            this.TunteinaLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TunteinaLB.Location = new System.Drawing.Point(209, 56);
+            this.TunteinaLB.Name = "TunteinaLB";
+            this.TunteinaLB.Size = new System.Drawing.Size(0, 25);
+            this.TunteinaLB.TabIndex = 2;
+            this.TunteinaLB.Visible = false;
+            // 
+            // KuukausinaLB
+            // 
+            this.KuukausinaLB.AutoSize = true;
+            this.KuukausinaLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KuukausinaLB.Location = new System.Drawing.Point(22, 91);
+            this.KuukausinaLB.Name = "KuukausinaLB";
+            this.KuukausinaLB.Size = new System.Drawing.Size(0, 25);
+            this.KuukausinaLB.TabIndex = 3;
+            this.KuukausinaLB.Visible = false;
+            // 
+            // MinuutteinaLB
+            // 
+            this.MinuutteinaLB.AutoSize = true;
+            this.MinuutteinaLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinuutteinaLB.Location = new System.Drawing.Point(209, 91);
+            this.MinuutteinaLB.Name = "MinuutteinaLB";
+            this.MinuutteinaLB.Size = new System.Drawing.Size(0, 25);
+            this.MinuutteinaLB.TabIndex = 4;
+            this.MinuutteinaLB.Visible = false;
+            // 
+            // PaivinaLB
+            // 
+            this.PaivinaLB.AutoSize = true;
+            this.PaivinaLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PaivinaLB.Location = new System.Drawing.Point(22, 126);
+            this.PaivinaLB.Name = "PaivinaLB";
+            this.PaivinaLB.Size = new System.Drawing.Size(0, 25);
+            this.PaivinaLB.TabIndex = 5;
+            this.PaivinaLB.Visible = false;
+            // 
+            // SekuntteinaLB
+            // 
+            this.SekuntteinaLB.AutoSize = true;
+            this.SekuntteinaLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SekuntteinaLB.Location = new System.Drawing.Point(209, 126);
+            this.SekuntteinaLB.Name = "SekuntteinaLB";
+            this.SekuntteinaLB.Size = new System.Drawing.Size(0, 25);
+            this.SekuntteinaLB.TabIndex = 6;
+            this.SekuntteinaLB.Visible = false;
+            // 
+            // LaskeBT
+            // 
+            this.LaskeBT.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LaskeBT.Location = new System.Drawing.Point(383, 18);
+            this.LaskeBT.Name = "LaskeBT";
+            this.LaskeBT.Size = new System.Drawing.Size(89, 35);
+            this.LaskeBT.TabIndex = 7;
+            this.LaskeBT.Text = "Laske";
+            this.LaskeBT.UseVisualStyleBackColor = true;
+            this.LaskeBT.Click += new System.EventHandler(this.LaskeBT_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LaskeBT);
+            this.Controls.Add(this.SekuntteinaLB);
+            this.Controls.Add(this.PaivinaLB);
+            this.Controls.Add(this.MinuutteinaLB);
+            this.Controls.Add(this.KuukausinaLB);
+            this.Controls.Add(this.TunteinaLB);
+            this.Controls.Add(this.VuosinaLB);
+            this.Controls.Add(this.SyntymaAikaDT);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker SyntymaAikaDT;
+        private System.Windows.Forms.Label VuosinaLB;
+        private System.Windows.Forms.Label TunteinaLB;
+        private System.Windows.Forms.Label KuukausinaLB;
+        private System.Windows.Forms.Label MinuutteinaLB;
+        private System.Windows.Forms.Label PaivinaLB;
+        private System.Windows.Forms.Label SekuntteinaLB;
+        private System.Windows.Forms.Button LaskeBT;
+    }
+}
+
---
+++ Harjoitus 3/Harjoitus 3/Harjoitus 4/Harjoitus 4/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Harjoitus_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LaskeBT_Click(object sender, EventArgs e)
+        {
+            DateTime synttari = SyntymaAikaDT.Value;
+            DateTime nyt = DateTime.Now;
+            double erotus = Math.Round((nyt - synttari).TotalDays);
+            VuosinaLB.Text = Math.Ceiling(erotus / 365.25) + " vuotta";
+            KuukausinaLB.Text = Math.Ceiling(erotus * 12 / 365.25) + " kuukautta";
+            PaivinaLB.Text = (erotus + " päivää");
+            TunteinaLB.Text = (erotus * 24 + " tuntia");
+            MinuutteinaLB.Text = (erotus * 24*60 + " minuuttia");
+            SekuntteinaLB.Text = (erotus * 24*3600 + " sekuntia");
+            VuosinaLB.Visible = true;
+            KuukausinaLB.Visible = true;
+            PaivinaLB.Visible = true;
+            TunteinaLB.Visible = true;
+            MinuutteinaLB.Visible = true;
+            SekuntteinaLB.Visible = true;
+        }
+    }
+}
