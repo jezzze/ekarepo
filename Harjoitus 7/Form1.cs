aa531fa60e342d430553219fb5e346bc9461f3b3
jezzze <69570708+jezzze@users.noreply.github.com>
2021-08-23 14:44:09 +0300

    Add files via upload

---
+++ Harjoitus 7/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Harjoitus_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MuutaBT_Click(object sender, EventArgs e)
+        {
+            if(FahrenheitToRB.Checked == true)
+            {
+                VastausLB.Text = Convert.ToString((Convert.ToInt16(MaaraTB.Text) + 5 / 9) - 32); 
+            }
+            if(CelsiusToRB.Checked == true)
+            {
+                VastausLB.Text = Convert.ToString((Convert.ToInt16(MaaraTB.Text) + 5 / 9) + 32);
+                VastausLB.Visible = true;
+            }
+            VastausLB.Visible = true;
+        }
+    }
+    
+
+}
