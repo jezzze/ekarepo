257b9aa73e87d0b74ee0031adcec08d8b0c96064
jezzze <69570708+jezzze@users.noreply.github.com>
2021-09-02 09:36:59 +0300

    Add files via upload

---
+++ RoomalaisetNumerot/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace RoomalaisetNumerot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SyotanumTB = new System.Windows.Forms.TextBox();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.NappiBT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(259, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Roomalaiset numerot";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(413, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Anna numero välillä 1-3999, niin muutan sen roomalaiseksi:";
+            // 
+            // SyotanumTB
+            // 
+            this.SyotanumTB.Location = new System.Drawing.Point(468, 128);
+            this.SyotanumTB.Name = "SyotanumTB";
+            this.SyotanumTB.Size = new System.Drawing.Size(127, 20);
+            this.SyotanumTB.TabIndex = 2;
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VastausLB.Location = new System.Drawing.Point(234, 219);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(51, 16);
+            this.VastausLB.TabIndex = 3;
+            this.VastausLB.Text = "label3";
+            this.VastausLB.Visible = false;
+            // 
+            // NappiBT
+            // 
+            this.NappiBT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NappiBT.Location = new System.Drawing.Point(397, 205);
+            this.NappiBT.Name = "NappiBT";
+            this.NappiBT.Size = new System.Drawing.Size(85, 40);
+            this.NappiBT.TabIndex = 4;
+            this.NappiBT.Text = "Muuta";
+            this.NappiBT.UseVisualStyleBackColor = true;
+            this.NappiBT.Click += new System.EventHandler(this.NappiBT_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 420);
+            this.Controls.Add(this.NappiBT);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.SyotanumTB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox SyotanumTB;
+        private System.Windows.Forms.Label VastausLB;
+        private System.Windows.Forms.Button NappiBT;
+    }
+}
+
