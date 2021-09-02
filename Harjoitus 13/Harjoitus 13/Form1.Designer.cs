5af4c1db00912739c5918aec179091d8084e9157
jezzze <69570708+jezzze@users.noreply.github.com>
2021-09-02 10:00:00 +0300

    Add files via upload

---
+++ Harjoitus 13/Harjoitus 13/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Harjoitus_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.NimiTB = new System.Windows.Forms.TextBox();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.TarkistaBT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(452, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anna nimesi, niin tarkistan, onko se 50 suosituimman joukossa:";
+            // 
+            // NimiTB
+            // 
+            this.NimiTB.Location = new System.Drawing.Point(476, 83);
+            this.NimiTB.Name = "NimiTB";
+            this.NimiTB.Size = new System.Drawing.Size(126, 20);
+            this.NimiTB.TabIndex = 1;
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VastausLB.Location = new System.Drawing.Point(12, 128);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(51, 20);
+            this.VastausLB.TabIndex = 2;
+            this.VastausLB.Text = "label2";
+            this.VastausLB.Visible = false;
+            // 
+            // TarkistaBT
+            // 
+            this.TarkistaBT.Location = new System.Drawing.Point(476, 121);
+            this.TarkistaBT.Name = "TarkistaBT";
+            this.TarkistaBT.Size = new System.Drawing.Size(100, 36);
+            this.TarkistaBT.TabIndex = 3;
+            this.TarkistaBT.Text = "Tarkista";
+            this.TarkistaBT.UseVisualStyleBackColor = true;
+            this.TarkistaBT.Click += new System.EventHandler(this.TarkistaBT_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 455);
+            this.Controls.Add(this.TarkistaBT);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.NimiTB);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NimiTB;
+        private System.Windows.Forms.Label VastausLB;
+        private System.Windows.Forms.Button TarkistaBT;
+    }
+}
+
---
+++ Harjoitus 13/Harjoitus 13/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Harjoitus_13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TarkistaBT_Click(object sender, EventArgs e)
+        {
+            VastausLB.Text = "";
+            VastausLB.Visible = false;
+            string[] pojat = File.ReadAllLines("C:/Users/jesse.mitronen/source/repos/Harjoitus 13/Harjoitus 13/pojat.txt");
+            string[] tytot = File.ReadAllLines("C:/Users/jesse.mitronen/source/repos/Harjoitus 13/Harjoitus 13/tytöt.txt");
+            string nimi = NimiTB.Text;
+            int laskurip = 1;
+            int laskurit = 1;
+            foreach (string poika in pojat)
+            {
+                if (nimi == poika)
+                {
+                    VastausLB.Text = "Nimesi on " + laskurip + ". suosituin poikien nimi vuonna 2020";
+                    VastausLB.Visible = true;
+                }
+                laskurip++;
+            }
+            foreach (string tytto in tytot)
+            {
+                if (nimi == tytto)
+                {
+                    VastausLB.Text = "Nimesi on " + laskurit + ". suosituin tyttöjen nimi vuonna 2020";
+                    VastausLB.Visible = true;
+                }
+                laskurit++;
+            }
+            if (VastausLB.Visible == false)
+            {
+                VastausLB.Text = "Nimesi ei löytynyt suosituimpien nimien joukosta! :-(";
+                VastausLB.Visible = true;
+            }
+        }
+    }
+}
