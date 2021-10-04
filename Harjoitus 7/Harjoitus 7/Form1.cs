de291eddd101afdffd0532dd4cf6f732a8be0642
jezzze <69570708+jezzze@users.noreply.github.com>
2021-10-04 12:23:15 +0300

    Add files via upload

---
+++ Harjoitus 7/Harjoitus 7/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Harjoitus_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MuutaBT_Click(object sender, EventArgs e)
+        {
+            double vastaus;
+            double asteet = Convert.ToDouble(MaaraTB.Text);
+            if (CelsiusToRB.Checked)
+            {
+                vastaus = asteet * 1.8 + 32;
+                VastausLB.Text = asteet + " Fahrenheitia on " + vastaus + " Celsius astetta";
+                VastausLB.Visible = true;
+
+            }
+            else if (FahrenheitToRB.Checked)
+            {
+                vastaus = (asteet - 32) / 1.8;
+                VastausLB.Text = asteet + " Celsiusta on " + vastaus + " Fahrenheit astetta";
+                VastausLB.Visible = true;
+            }
+            else
+            {
+                VastausLB.Text = "Et valinnut muunnettavaa astetta tai antanut oikeaa lukua";
+                VastausLB.Visible = true;
+            }
+        }
+    }
+    
+
+}
